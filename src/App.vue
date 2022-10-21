82679b8da3ec4e6fc2e5f7faf340b372b9e1db0a
Juan_Velez <esteban.velez@alternova.co>
2022-10-21 11:13:22 -0500

    initial structure with vite

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 >Hola Hydra</h1>
+  <button @click="changeColor($event)">Cambiar Color</button>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+function changeColor(event: Event): void {
+  console.log(event.target)
+  return
+}
+
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
